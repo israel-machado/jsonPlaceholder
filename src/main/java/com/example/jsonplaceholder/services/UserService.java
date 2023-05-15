50b85a88c3c5893c6fba8e52b88f232733fcb7dd
Israel Machado <israelmach26@gmail.com>
2023-05-15 09:52:07 -0300

    Exceptions for UserService

--- src/main/java/com/example/jsonplaceholder/services/UserService.java
+++ src/main/java/com/example/jsonplaceholder/services/UserService.java
@@ -5,7 +5,13 @@ import com.example.jsonplaceholder.model.domain.user.UserDomain;
 import com.example.jsonplaceholder.model.dto.request.UserRequest;
 import com.example.jsonplaceholder.model.dto.response.UserResponse;
 import com.example.jsonplaceholder.repository.UserRepository;
+import com.example.jsonplaceholder.services.exceptions.DatabaseException;
+import com.example.jsonplaceholder.services.exceptions.InvalidParameterException;
+import com.example.jsonplaceholder.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -26,23 +32,40 @@ public class UserService {
 
     public UserResponse findUserById(Long id) {
         Optional<UserDomain> user = userRepository.findById(id);
-        return user.map(UserConverter::convertUserDomainToUserResponse).orElse(null);
+        return user.map(UserConverter::convertUserDomainToUserResponse)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + id));
     }
 
     public UserResponse insertUser(UserRequest userRequest) {
+        if (userRequest == null) {
+            throw new InvalidParameterException("User request cannot be null");
+        }
         UserDomain user = convertUserRequestToDomain(userRequest);
         user = userRepository.save(user);
         return convertUserDomainToUserResponse(user);
     }
 
     public UserResponse updateUser(Long id, UserRequest userRequest) {
-        UserDomain user = convertUserRequestToDomain(userRequest);
-        user.setId(id);
-        UserDomain updatedUser = userRepository.save(user);
-        return convertUserDomainToUserResponse(updatedUser);
+        try {
+            if (userRequest == null) {
+                throw new InvalidParameterException("User request cannot be null");
+            }
+            UserDomain user = convertUserRequestToDomain(userRequest);
+            user.setId(id);
+            UserDomain updatedUser = userRepository.save(user);
+            return convertUserDomainToUserResponse(updatedUser);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("User not found with id: " + id);
+        }
     }
 
     public void deleteUser(Long id) {
-        userRepository.deleteById(id);
+        try {
+            userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("User not found with id: " + id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Error deleting user with id: " + id + " - " + e.getMessage());
+        }
     }
 }
---
+++ src/main/java/com/example/jsonplaceholder/services/exceptions/InvalidParameterException.java
@@ -0,0 +1,7 @@
+package com.example.jsonplaceholder.services.exceptions;
+
+public class InvalidParameterException extends RuntimeException {
+    public InvalidParameterException(String message) {
+        super(message);
+    }
+}
